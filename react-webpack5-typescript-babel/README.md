2baffce680513848d9a20074e7f8b8584ed49978
XING YAHAO <keidarcy.1015@gmail.com>
2020-10-18 13:42:14 +0900

    add react typescript babel-loader

---
+++ react-webpack5-typescript-babel/README.md
@@ -0,0 +1,1 @@
+[link](https://www.carlrippon.com/creating-react-and-typescript-apps-with-webpack/)
---
+++ react-webpack5-typescript-babel/src/Sub.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Sub: React.FC = ({}) => {
+  return <>nice</>;
+};
