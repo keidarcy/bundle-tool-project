b36d9ee747247614af8d3267e1103bc4b58e9238
XING YAHAO <keidarcy.1015@gmail.com>
2021-06-06 17:35:05 +0900

    added vite example

--- webpack-react-sass/src/main.js
+++ webpack-react-sass/src/main.js
@@ -1,16 +1,22 @@
 import { v4 } from 'uuid';
 import './style/main.scss';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import { Yoyo } from './components/Yoyo';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const handleIncrement = useCallback(() => {
+    setCount((c) => c + 2);
+  });
   return (
     <>
       <h1 className="hello">HELLO from react</h1>
       <p>nice</p>
       <section>{v4()}</section>
-      <Yoyo name="haha" />
+      <p>{count}</p>
+      <button onClick={handleIncrement}>increment</button>
+      <Yoyo name="ha not bad" />
     </>
   );
 };
